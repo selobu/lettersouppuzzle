6f0433e5590945d0da6aa081681fdb619c00cdca
Sebastian Lopez Buritica <selobu@gmail.com>
2022-12-19 09:17:45 -0500

    feat: :heavy_plus_sign: added errror module
    adde error module and some error codes

---
+++ modules/errors.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+import difflib # ref 
+
+
+ERROR_CODES={'401':'',
+             '402':'',
+             '403':'',}
+
+class ErrorCode(Exception):
+    def __init__(self, code):
+        if code not in ERROR_CODES.keys():
+            closed_error_code =  difflib.get_close_matches(code, ERROR_CODES.keys())
+            raise Exception(f'Unknown error code: {code}\npossible you would like to set {closed_error_code}')
+        self.code = code
+
