6024bc48b95f7577f2a65e6a266d3c99ce0b5096
Sebastian Lopez Buritica <selobu@gmail.com>
2022-12-19 10:29:01 -0500

    refactor: :recycle: create a class to store and test crossword data

--- modules/modulesconfig.py
+++ modules/modulesconfig.py
@@ -3,4 +3,6 @@ ERROR_CODES = {
     "402": "Not iterable row ",
     "403": "Not consistent columns elements ",
     "404": "Not allowed character ",
+    "405": "Minimum rows 4",
+    "406": "Minimum cols 4",    
 }
--- modules/readData.py
+++ modules/readData.py
@@ -2,77 +2,87 @@
 from errors import ErrorCode
 
 
-def __checkifiterable__(matrix: list) -> bool:
-    """Check if matriz is iterable
-
-    Args:
-        matrix: iterable of iterables which contains letters from A-Z
-
-    Return:
-        Boolean: True if all rows are iterable otherwise raise a error
-    """
-    if not hasattr(matrix, "__iter__"):
-        raise ErrorCode(401)
-
-    # checking if row elements are iterable
-    for pos, row in enumerate(matrix):
-        if not hasattr(row, "__iter__"):
-            ErrorCode(402, message=f"row:{pos}")
-
-
-def __get_matrix_dimensions__(matrix: list) -> list:
-    """Check if the matrix dimensions are consistent
-    Minimum rows 4, minimum columns 4
-
-    Args:
-        matrix: iterable of iterables which contains letters from A-Z
-
-    Return:
-        list: matrix dimensions rows x columns, otherwise raise a error
-    """
-
-    # check if matrix is iterable, if not the function automatically dispatch a error
-    __checkifiterable__(matrix)
-
-    rows_len = [len(row) for row in matrix]
-    rows = len(rows_len)
-    columns = rows_len[0]
-
-    not_consisten_rows = [pos for pos, i in enumerate(rows_len) if len(i) == columns]
-    if len(not_consisten_rows) > 0:
-        not_consisten_rows = "".join(not_consisten_rows)
-        raise ErrorCode(403, f"Failing rows: { not_consisten_rows }")
-    return (rows, columns)
-
-
-def __check_single_character__(character: str):
-    """Given a character check if it's allowed"""
-    # converting to uppercase and check equivalent number
-    number = ord(character.upper())
-    if 65 <= number <= 90:
-        raise ErrorCode(404, f"{character}")
-
-
-def __testRowContent__(row: list) -> None:
-    """Test if all row elements are considered as an english word
-
-    Args:
-        row (list): list of charactes bettween A-Z
-
-    Returns:
-        bool: True if all elements are valid else raise a error
-    """
-    for character in row:
-        __check_single_character__(character)
-
-
-def readData(matrix: list) -> None:
+class CrosswordData:
     """Read  the contents of the matrix and stores as a global variable
 
     Args:
         matriz: iterable of iterables which contains letters from A-Z
+        Minimum dmensions: rows 4, columns 4"
 
     Return:
         None
     """
-    pass
+
+    def __init__(self, matrix: list):
+        self._nrows = 0
+        self._ncols = 0
+        self.valid = False  # switch to indicate if matrix data is valid or not
+        self.matrix = matrix
+        self.__checkifiterable__()
+        self.nrows, self.ncols = self.__get_matrix_dimensions__()
+        for row in self.matrix:
+            self.__testRowContent__(row)
+
+    @property
+    def nrows(self):
+        return self._nrows
+
+    @nrows.setter
+    def nrows(self, nrows):
+        if nrows <= 4:
+            raise ErrorCode(405)
+        self._nrows = nrows
+
+    @property
+    def ncols(self):
+        return self._ncols
+
+    @ncols.setter
+    def ncols(self, ncols):
+        if ncols <= 4:
+            raise ErrorCode(405)
+        self._cols = ncols
+
+    def __checkifiterable__(self):
+        """Check if matriz is iterable, raise a error if not"""
+        if not hasattr(self.matrix, "__iter__"):
+            raise ErrorCode(401)
+
+        # checking if row elements are iterable
+        for pos, row in enumerate(self.matrix):
+            if not hasattr(row, "__iter__"):
+                ErrorCode(402, message=f"row:{pos}")
+
+    def __check_single_character__(character: str):
+        """Given a character check if it's allowed, raise a error if not"""
+        # converting to uppercase and check equivalent number
+        number = ord(character.upper())
+        if 65 <= number <= 90:
+            raise ErrorCode(404, f"{character}")
+
+    def __get_matrix_dimensions__(self) -> list:
+        """Check if the matrix dimensions are consistent, , raise a error if not"""
+
+        rows_len = [len(row) for row in self.matrix]
+        rows = len(rows_len)
+        columns = rows_len[0]
+
+        not_consisten_rows = [
+            pos for pos, i in enumerate(rows_len) if len(i) == columns
+        ]
+        if len(not_consisten_rows) > 0:
+            not_consisten_rows = "".join(not_consisten_rows)
+            raise ErrorCode(403, f"Failing rows: { not_consisten_rows }")
+        return (rows, columns)
+
+    def __testRowContent__(self, row: list) -> None:
+        """Test if all row elements are considered as an english word
+
+        Args:
+            row (list): list of charactes bettween A-Z
+
+        Returns:
+            bool: True if all elements are valid else raise a error
+        """
+        for character in row:
+            self.__check_single_character__(character)
