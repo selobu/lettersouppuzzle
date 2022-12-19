55265e7aff395284dfcadac934da76a1d9a8ea29
Sebastian Lopez Buritica <selobu@gmail.com>
2022-12-19 11:52:19 -0500

    fix black in tests

--- tests/test_modules_readData.py
+++ tests/test_modules_readData.py
@@ -4,81 +4,98 @@ from modules.errors import ErrorCode
 
 
 def test_invalid_matriz():
-    matrix= 1
+    matrix = 1
     try:
         CrosswordData(matrix)
     except Exception as exc:
-        e = exc    
-    
+        e = exc
+
     assert e.__class__ == ErrorCode
-    assert e.code == '401'
+    assert e.code == "401"
+
 
 def test_not_iterable_row():
-    matrix= [1,['a','b','c']]
+    matrix = [1, ["a", "b", "c"]]
     try:
         CrosswordData(matrix)
     except Exception as exc:
-        e = exc    
-    
+        e = exc
+
     assert e.__class__ == ErrorCode
-    assert e.code == '402'
-    
+    assert e.code == "402"
+
+
 def test_invalid_matriz_dict():
-    matrix= {}
+    matrix = {}
     try:
         CrosswordData(matrix)
     except Exception as exc:
-        e = exc    
-    
+        e = exc
+
     assert e.__class__ == ErrorCode
-    assert e.code == '407'
-    
+    assert e.code == "407"
+
+
 def test_empty_matriz():
-    matrix= []
+    matrix = []
     try:
         CrosswordData(matrix)
     except Exception as exc:
-        e = exc    
-    
+        e = exc
+
     assert e.__class__ == ErrorCode
-    assert e.code == '407'
+    assert e.code == "407"
+
 
 def test_single_row_matrix():
-    matrix= [['A','B','C']]
+    matrix = [["A", "B", "C"]]
     try:
         CrosswordData(matrix)
     except Exception as exc:
-        e = exc    
-    
+        e = exc
+
     assert e.__class__ == ErrorCode
-    assert e.code == '405'
+    assert e.code == "405"
+
 
 def test_small_matrix():
-    matrix= [['A','B','C'],['C','D','E'],['F','G','H']]
+    matrix = [["A", "B", "C"], ["C", "D", "E"], ["F", "G", "H"]]
     try:
         CrosswordData(matrix)
     except Exception as exc:
-        e = exc    
-    
+        e = exc
+
     assert e.__class__ == ErrorCode
-    assert e.code == '405'
-    
+    assert e.code == "405"
+
+
 def test_matrix_missing_column():
-    matrix= [['A','B','C','D'],['C','D','E','F'],['F','G','H'], ['F','G','H','U']]
+    matrix = [
+        ["A", "B", "C", "D"],
+        ["C", "D", "E", "F"],
+        ["F", "G", "H"],
+        ["F", "G", "H", "U"],
+    ]
     try:
         CrosswordData(matrix)
     except Exception as exc:
-        e = exc    
-    
+        e = exc
+
     assert e.__class__ == ErrorCode
-    assert e.code == '403'
-    
+    assert e.code == "403"
+
+
 def test_matrix_not_allowed_character():
-    matrix= [['A','B','C','D'],['C','D','E','F'],['F','G','H','ñ'], ['F','G','H','ñ']]
+    matrix = [
+        ["A", "B", "C", "D"],
+        ["C", "D", "E", "F"],
+        ["F", "G", "H", "ñ"],
+        ["F", "G", "H", "ñ"],
+    ]
     try:
         CrosswordData(matrix)
     except Exception as exc:
-        e = exc    
-    
+        e = exc
+
     assert e.__class__ == ErrorCode
-    assert e.code == '404'
+    assert e.code == "404"
