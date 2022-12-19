469b05160ad13142bbceaf6a74ef72429c70c058
Sebastian Lopez Buritica <selobu@gmail.com>
2022-12-19 09:31:23 -0500

    feat: :heavy_plus_sign: Add custom message to ErrorCode

--- modules/errors.py
+++ modules/errors.py
@@ -3,10 +3,11 @@ import difflib # ref
 from modulesconfig import ERROR_CODES
 
 class ErrorCode(Exception):
-    def __init__(self, code):
+    def __init__(self, code, message=None):
         if code not in ERROR_CODES.keys():
             closed_error_code =  difflib.get_close_matches(code, ERROR_CODES.keys())
             raise Exception(f'Unknown error code: {code}\npossible you would like to set {closed_error_code}')
         self.code = code
+        self.message = message
     def __str__(self):
-        return f'{self.code}: {ERROR_CODES[self.code]}'
+        return f'{self.code}: {ERROR_CODES[self.code]} {self.message}'
