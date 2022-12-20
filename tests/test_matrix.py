d0b266b9f4445d2796d7e5176f07b051f37f17ad
Sebastian Lopez Buritica <selobu@gmail.com>
2022-12-19 20:44:40 -0500

    runing black

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -9,7 +9,7 @@ matrix = [
     "XAJGUHV",
     "FMOROLB",
     "gAHJENE",
-    ]
+]
 matrix = [[u for u in s.upper()] for s in matrix]
 data = CrosswordData(matrix)
 
@@ -17,39 +17,70 @@ data = CrosswordData(matrix)
 def test_search1():
     word2search = "USG"
     founded = findWord(word2search, data)
-    assert str(founded) == '[Positions(initpoint=Point(row=1, col=2), endpoint=Point(row=3, col=4))]'
+    assert (
+        str(founded)
+        == "[Positions(initpoint=Point(row=1, col=2), endpoint=Point(row=3, col=4))]"
+    )
+
 
 def test_search2():
     word2search = "GSU"
     founded = findWord(word2search, data)
-    assert str(founded) == '[Positions(initpoint=Point(row=1, col=2), endpoint=Point(row=3, col=4))]'
+    assert (
+        str(founded)
+        == "[Positions(initpoint=Point(row=1, col=2), endpoint=Point(row=3, col=4))]"
+    )
+
 
 def test_search3():
     word2search = "RAGO"
     founded = findWord(word2search, data)
-    assert str(founded) == '[Positions(initpoint=Point(row=1, col=1), endpoint=Point(row=4, col=4))]'
+    assert (
+        str(founded)
+        == "[Positions(initpoint=Point(row=1, col=1), endpoint=Point(row=4, col=4))]"
+    )
+
 
 def test_search4():
     word2search = "OGAR"
     founded = findWord(word2search, data)
-    assert str(founded) == '[Positions(initpoint=Point(row=1, col=1), endpoint=Point(row=4, col=4))]'
-    
+    assert (
+        str(founded)
+        == "[Positions(initpoint=Point(row=1, col=1), endpoint=Point(row=4, col=4))]"
+    )
+
+
 def test_search5():
     word2search = "CASA"
     founded = findWord(word2search, data)
-    assert str(founded) == '[Positions(initpoint=Point(row=2, col=1), endpoint=Point(row=2, col=4))]'
+    assert (
+        str(founded)
+        == "[Positions(initpoint=Point(row=2, col=1), endpoint=Point(row=2, col=4))]"
+    )
+
 
 def test_search6():
     word2search = "ASAC"
     founded = findWord(word2search, data)
-    assert str(founded) == '[Positions(initpoint=Point(row=2, col=1), endpoint=Point(row=2, col=4))]'
+    assert (
+        str(founded)
+        == "[Positions(initpoint=Point(row=2, col=1), endpoint=Point(row=2, col=4))]"
+    )
+
 
 def test_search7():
     word2search = "EHOJ"
     founded = findWord(word2search, data)
-    assert str(founded) == '[Positions(initpoint=Point(row=5, col=3), endpoint=Point(row=2, col=6))]'
+    assert (
+        str(founded)
+        == "[Positions(initpoint=Point(row=5, col=3), endpoint=Point(row=2, col=6))]"
+    )
+
 
 def test_search8():
     word2search = "JOHE"
     founded = findWord(word2search, data)
-    assert str(founded) == '[Positions(initpoint=Point(row=5, col=3), endpoint=Point(row=2, col=6))]'
+    assert (
+        str(founded)
+        == "[Positions(initpoint=Point(row=5, col=3), endpoint=Point(row=2, col=6))]"
+    )
