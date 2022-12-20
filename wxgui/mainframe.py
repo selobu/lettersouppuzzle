698285744d82faddba2cd0815093c40ba40dc191
Sebastian Lopez Buritica <selobu@gmail.com>
2022-12-20 05:32:35 -0500

    feat: :heavy_plus_sign: properly displaying matrix data

--- wxgui/mainframe.py
+++ wxgui/mainframe.py
@@ -3,6 +3,8 @@ from configuration import config
 import wx.lib.agw.xlsgrid as XG
 import wx.grid as gridlib
 
+from modules.readData import CrosswordData
+from modules.findword import findWord
 
 class MainFrame(wx.Frame):
     no_caption = [wx.DEFAULT_FRAME_STYLE, wx.NO_FULL_REPAINT_ON_RESIZE][0]
@@ -23,12 +25,26 @@ class MainFrame(wx.Frame):
         self.CreateMenuAndStatusBar()
         self.CenterOnScreen()
         self.Layout()
+        wx.CallAfter(self.populateinitdata)
 
     def create_ui(self):
-        panel = XLSGridPanel(self)
+        self.sheet = SimpleGrid(self)
 
     def CreateMenuAndStatusBar(self):
         pass
+    
+    def populateinitdata(self, *args):
+        matrix = [
+            "DEYQAUG",
+            "XRGTUAV",
+            "SCASABE",
+            "XAJGUHV",
+            "FMOROLB",
+            "gAHJENE",
+        ]
+        matrix = [[u for u in s.upper()] for s in matrix]
+        data = CrosswordData(matrix)
+        self.sheet.display(data)
 
 
 class SimpleGrid(gridlib.Grid):
@@ -39,10 +55,21 @@ class SimpleGrid(gridlib.Grid):
 
     def create(self, nrows=25, ncols=25):
         self.CreateGrid(nrows, ncols)
-
-    def setmatrixdata(self, matrix):
+        
+    def display(self, data):
+        ccols = self.GetNumberCols()
+        crows = self.GetNumberRows()
+        if crows > 0:
+            self.DeleteRows(0, crows)
+        if ccols > 0:
+            self.DeleteCols(0, ccols)
+        self.InsertRows(0,data.nrows)
+        self.InsertCols(0,data.ncols)
+        self.setmatrixdata(data)
+        
+    def setmatrixdata(self, matrix:CrosswordData):        
         for rownumber, row in enumerate(matrix):
-            for colnumber, element in row:
+            for colnumber, element in enumerate(row):
                 self.SetCellValue(rownumber, colnumber, str(element))
 
 
