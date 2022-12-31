8c5ec8c3ef68def5ad26596b9b321beb5d5efcf4
Sebastian Lopez Buritica <selobu@gmail.com>
2022-12-31 16:26:16 -0500

    fix: :adhesive_bandage: adding missing return character, related to style check

--- modules/test01.py
+++ modules/test01.py
@@ -1,33 +1,37 @@
 # coding:utf-8
 from random import randint
 
-inputvector = [randint(-5,5) for i in range(10)]
-print('Input values\n:')
+inputvector = [randint(-5, 5) for i in range(10)]
+print("Input values\n:")
 print(inputvector)
 
-def mymin(u:list=[]):
+
+def mymin(u: list = []):
     minimum = u[0]
     for i in u:
-        if i<minimum:
+        if i < minimum:
             minimum = i
     return minimum
-    
-def remove_element(element, u:list)->list:
+
+
+def remove_element(element, u: list) -> list:
     newarray = u[:]
     for position, i in enumerate(u):
         if i == element:
             newarray.pop(position)
             return newarray
 
-def ordenar(data: list)->list:
+
+def ordenar(data: list) -> list:
     newdata = data[:]
     res = []
     for elements in range(len(data)):
         res.append(mymin(newdata))
         newdata = remove_element(res[-1], newdata)
-    return res 
+    return res
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # testing
     # print(mymin([1,2,2,2,1,1,2]))
     # print(remove_element(4, [0,1,2,3,4]))
